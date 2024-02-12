dc02d1a21d664da111ce278e32167581abc8bec6
Semyon Levin <levin.semen@gmail.com>
2024-02-12 14:21:26 -0800

    Initial files

--- src/main/java/name/remal/gradle_plugins/dependency_versions_retriever/Downloader.java
+++ src/main/java/name/remal/gradle_plugins/dependency_versions_retriever/Downloader.java
@@ -1,12 +1,17 @@
 package name.remal.gradle_plugins.dependency_versions_retriever;
 
+import static java.nio.file.Files.copy;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static lombok.AccessLevel.PRIVATE;
 import static name.remal.gradle_plugins.toolkit.ObjectUtils.isEmpty;
+import static name.remal.gradle_plugins.toolkit.PathUtils.createParentDirectories;
+import static name.remal.gradle_plugins.toolkit.PathUtils.deleteRecursively;
 import static name.remal.gradle_plugins.toolkit.PathUtils.normalizePath;
 
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.annotation.Nullable;
 import lombok.CustomLog;
 import lombok.NoArgsConstructor;
@@ -30,6 +35,7 @@ abstract class Downloader {
         downloadToFile(url, targetFile.toPath(), metadataFile != null ? metadataFile.toPath() : null);
     }
 
+    @SneakyThrows
     public static void downloadToFile(URL url, Path targetPath, @Nullable Path metadataPath) {
         val protocol = url.getProtocol();
         if (isEmpty(protocol)) {
@@ -45,7 +51,32 @@ abstract class Downloader {
             logger.debug("Downloading {} into {} (metadata will be saved into {})", url, targetPath, metadataPath);
         }
 
+        deleteRecursively(targetPath);
+        if (metadataPath != null) {
+            deleteRecursively(metadataPath);
+        }
 
+        if (protocol.equalsIgnoreCase("file")) {
+            val sourcePath = Paths.get(url.toURI());
+            copyFile(sourcePath, targetPath, metadataPath);
+
+        } else if (protocol.equalsIgnoreCase("http")
+            || protocol.equalsIgnoreCase("https")
+        ) {
+            downloadHttpToFile(url, targetPath, metadataPath);
+
+        } else {
+            throw new UnsupportedOperationException("Unsupported protocol: " + protocol);
+        }
+    }
+
+    @SneakyThrows
+    private static void copyFile(Path sourcePath, Path targetPath, @Nullable Path metadataPath) {
+        createParentDirectories(targetPath);
+        copy(sourcePath, targetPath, REPLACE_EXISTING);
+    }
+
+    private static void downloadHttpToFile(URL url, Path targetPath, @Nullable Path metadataPath) {
     }
 
 }
