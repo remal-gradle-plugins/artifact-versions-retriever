7dcb78988400b198226358bde80c7ab345431cbc
Semyon Levin <levin.semen@gmail.com>
2024-02-14 17:09:06 -0800

    fix build

--- src/main/java/name/remal/gradle_plugins/versions_retriever/git/AbstractRetrieveVersionsFromGit.java
+++ src/main/java/name/remal/gradle_plugins/versions_retriever/git/AbstractRetrieveVersionsFromGit.java
@@ -1,11 +1,17 @@
 package name.remal.gradle_plugins.versions_retriever.git;
 
 import name.remal.gradle_plugins.versions_retriever.AbstractRetrieveVersionsWithWorkerExecutor;
+import org.gradle.api.JavaVersion;
 
 public abstract class AbstractRetrieveVersionsFromGit extends AbstractRetrieveVersionsWithWorkerExecutor {
 
     {
         getOutputs().cacheIf("This task should always fetch from remote Git repository", __ -> false);
+    }
+
+    @Override
+    protected JavaVersion getMinSupportedJavaVersion() {
+        return JavaVersion.VERSION_11;
     }
 
 }
